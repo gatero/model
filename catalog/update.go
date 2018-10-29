e96aba9f1de6dc9499448f2530ee7cc4aa7014c5
Daniel Ortega <contact@daniel-ortega.mx>
2018-10-28 21:11:31 -0600

    use new db package in  update method

--- catalog/update.go
+++ catalog/update.go
@@ -2,7 +2,6 @@ package catalog
 
 import (
 	pb "app/grpc"
-	"app/mongo"
 	"context"
 
 	"google.golang.org/grpc/codes"
@@ -11,46 +10,46 @@ import (
 )
 
 func (rpc *RPC) Update(context context.Context, request *pb.UpdateRequest) (*pb.Response, error) {
-	dataType := request.Data.Type
-	dataId := request.Data.Id
-	dataAttributes := request.Data.Attributes
-	query := bson.M{
-		"_id": bson.ObjectIdHex(dataId),
-	}
-	collection, err := mongo.GetCollection(dataType)
-	if err != nil {
+	rpc.Mongo.CollectionName = request.Data.Type
+	if err := rpc.Mongo.SetCollection(); err != nil {
 		return nil, err
 	}
+	defer rpc.Mongo.Session.Close()
 
-	var instancetoUpdate map[string]interface{}
-	if err := collection.Find(query).One(&instancetoUpdate); err != nil {
+	dataAttributes := request.Data.Attributes
+	query := bson.M{
+		"_id": bson.ObjectIdHex(request.Data.Type),
+	}
+
+	var instanceToUpdate map[string]interface{}
+	if err := rpc.Mongo.Collection.Find(query).One(&instanceToUpdate); err != nil {
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 
-	if instancetoUpdate == nil {
+	if instanceToUpdate == nil {
 		return nil, status.Errorf(codes.NotFound, "Not found")
 	}
 
 	for key, _ := range dataAttributes {
 		if key != "" && key != "_id" {
-			instancetoUpdate[key] = dataAttributes[key]
+			instanceToUpdate[key] = dataAttributes[key]
 		}
 	}
 
-	if err := collection.Update(query, &instancetoUpdate); err != nil {
+	if err := rpc.Mongo.Collection.Update(query, &instanceToUpdate); err != nil {
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 
 	outputAttributes := make(map[string]string)
-	for key, value := range instancetoUpdate {
+	for key, value := range instanceToUpdate {
 		if value != nil && key != "_id" {
 			outputAttributes[key] = value.(string)
 		}
 	}
 
 	return &pb.Response{
-		Type:       dataType,
-		Id:         dataId,
+		Type:       request.Data.Type,
+		Id:         request.Data.Id,
 		Attributes: outputAttributes,
 	}, nil
 }
